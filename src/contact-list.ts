547b3510f5969a79a16bec03155848d1d6980494
Alok Saldanha <aloksaldanha@gmail.com>
2022-12-30 11:49:40 -0500

    initial import of Aurelia contact manager

---
+++ src/contact-list.ts
@@ -0,0 +1,18 @@
+import { inject } from 'aurelia-framework';
+import { WebAPI } from "web-api";
+@inject(WebAPI)
+export class ContactList {
+  contacts;
+  selectedId = 0;
+
+  constructor(private api: WebAPI) { }
+  created() { 
+    this.api.getContactList().then(contacts => this.contacts =contacts);
+
+  }
+
+  select(contact) {
+    this.selectedId = contact.id;
+    return true;
+  }
+}
